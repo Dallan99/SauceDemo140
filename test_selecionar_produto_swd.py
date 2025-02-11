cca14ca5d5caddc543777d85873eefc7dbe10213
Dallan99 <dallanr@gmail.com>
2025-02-11 18:17:42 -0300

    Verificações da página de inventário

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -1,16 +1,16 @@
 #  1 - Bibliotecas
 import selenium
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.common.by import By
 
 # 2 - Classe (Opcional)
 class Teste_Produtos():
 
-    # 2.1 Atribuitos
+    # 2.1 Atributos
     url = "https://www.saucedemo.com"        # endereço do site alvo
 
-    #2.2 Funções e Métodos
-    def setup_method(self, method ):               # ou def iniciar(method): TAMBÉM FUNCIONA  // Método de inicualização dos testes
+    # 2.2 Funções e Métodos
+    def setup_method(self, method):               # ou def iniciar(method): TAMBÉM FUNCIONA  // Método de inicualização dos testes
         self.driver = webdriver.Chrome()    # instacia o objeto do Selenium WbeDriver como Chrome
         self.driver.implicitly_wait(10)    # Define o tempo de espera padrão em 10 segundos
 
@@ -21,5 +21,10 @@ class Teste_Produtos():
         self.driver.get(self.url)                # Abre o navegador
         self.driver.find_element(By.ID, "user-name").send_keys("standard_user")  # user name é o nome do local e o standard user é o login
         self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")   #escreve a senha
-        self.driver.find_element(By.ID, "login-button").click()  # Clica no botão de login
-1
+        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click()# Clica no botão de login
+        #transição de página
+
+        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"  # confirma se está escrito Products no elemento
+        assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack" # confirma se é a mochila
+        # confirma o preço da mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99" 
